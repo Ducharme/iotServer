498b4df353efad6391265a1b1195be7a3e3d4924
claude-at-748293476463 <claude.ducharme@gmail.com>
2022-10-11 20:25:06 +0800

    Put certs in sub folder iotServer on s3

--- refresh-local-certs.sh
+++ refresh-local-certs.sh
@@ -5,8 +5,8 @@ AWS_ACCOUNT_ID_VALUE=$(aws sts get-caller-identity --query "Account" --output te
 S3_OBJECT_STORE=$PROJECT_NAME-object-store-$AWS_ACCOUNT_ID_VALUE
 
 mkdir ./certs
-aws s3 cp s3://$S3_OBJECT_STORE/certs/certificate-id.txt ./certs
-aws s3 cp s3://$S3_OBJECT_STORE/certs/certificate.pem.crt ./certs
-aws s3 cp s3://$S3_OBJECT_STORE/certs/private.pem.key ./certs
-aws s3 cp s3://$S3_OBJECT_STORE/certs/public.pem.key ./certs
-aws s3 cp s3://$S3_OBJECT_STORE/certs/root-ca.crt ./certs
+aws s3 cp s3://$S3_OBJECT_STORE/certs/iotServer/certificate-id.txt ./certs
+aws s3 cp s3://$S3_OBJECT_STORE/certs/iotServer/certificate.pem.crt ./certs
+aws s3 cp s3://$S3_OBJECT_STORE/certs/iotServer/private.pem.key ./certs
+aws s3 cp s3://$S3_OBJECT_STORE/certs/iotServer/public.pem.key ./certs
+aws s3 cp s3://$S3_OBJECT_STORE/certs/iotServer/root-ca.crt ./certs
