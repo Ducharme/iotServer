537c11da80ec52fc13be271ec08cf0e348bdd5d6
claude-at-748293476463 <claude.ducharme@gmail.com>
2022-10-13 20:26:31 +0800

    Fix unexpected format messages

--- session.ts
+++ session.ts
@@ -31,28 +31,33 @@ class Session {
         const decoder = new TextDecoder('utf8');
 
         const onPublish = async (receivedTopic: string, payload: ArrayBuffer, dup: boolean, qos: mqtt.QoS, retain: boolean) => {
-            const text = decoder.decode(payload);
-            const json = JSON.parse(text);
-            const deviceId = json.deviceId;
-            console.log(`Message received on topic ${receivedTopic} ${JSON.stringify(json)} (dup:${dup} qos:${qos} retain:${retain})`);
+            var text = "";
+            try {
+                text = decoder.decode(payload);
+                const json = JSON.parse(text);
+                const deviceId = json.deviceId;
+                console.log(`Message received on topic ${receivedTopic} ${JSON.stringify(json)} (dup:${dup} qos:${qos} retain:${retain})`);
 
-            const tokens = this.requestTopic.split("+");
-            const devIdFromTopic = receivedTopic.replace(tokens[0], "").replace(tokens[1], "");
+                const tokens = this.requestTopic.split("+");
+                const devIdFromTopic = receivedTopic.replace(tokens[0], "").replace(tokens[1], "");
 
-            if (deviceId != devIdFromTopic) {
-                console.warn(`deviceIds do not match with json.deviceId=${deviceId} devIdFromTopic=${devIdFromTopic}, skipping reply`);
-                return;
+                if (deviceId != devIdFromTopic) {
+                    console.warn(`deviceIds do not match with json.deviceId=${deviceId} devIdFromTopic=${devIdFromTopic}, skipping reply`);
+                    return;
+                }
+
+                // Reply to the request
+                var topicReply  = this.replyTopic.replace("+", deviceId);
+                const streamId = 0; // TODO: Get streamId from Redis
+                const seqId = 0; // TODO: Get seq from Redis
+                const msg = { deviceId: deviceId, streamId: streamId, seq: seqId, serverId: this.clientId };
+                const jsonReply = JSON.stringify(msg);
+                console.log(`Publishing to ${topicReply} message ${jsonReply}`);
+                var res = await this.conn.publish(topicReply, jsonReply, mqtt.QoS.AtLeastOnce);
+                console.log(`Published to ${topicReply} returned ${JSON.stringify(res)}`);
+            } catch (ex) {
+                console.error(`Failed to process received message "${text}" -> ${ex}`);
             }
-
-            // Reply to the request
-            var topicReply  = this.replyTopic.replace("+", deviceId);
-            const streamId = 0; // TODO: Get streamId from Redis
-            const seqId = 0; // TODO: Get seq from Redis
-            const msg = { deviceId: deviceId, streamId: streamId, seq: seqId, serverId: this.clientId };
-            const jsonReply = JSON.stringify(msg);
-            console.log(`Publishing to ${topicReply} message ${jsonReply}`);
-            var res = await this.conn.publish(topicReply, jsonReply, mqtt.QoS.AtLeastOnce);
-            console.log(`Published to ${topicReply} returned ${JSON.stringify(res)}`);
         };
 
         var res = await this.conn.subscribe(this.requestTopic, mqtt.QoS.AtLeastOnce, onPublish);
